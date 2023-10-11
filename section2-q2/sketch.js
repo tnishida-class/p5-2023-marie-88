69f371cf73106ce741c148901c5e6853145942e7
marie-88 <yui_n0mura@yahoo.co.jp>
2023-10-12 00:30:02 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,24 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      let x = size * i
+      let y = size * j
+    if((x+y)%2==1){
+      fill(100);
+      rect (x,y,size,size);
+    }
+    else{
+      fill(500)
+      rect (x,y,size,size);
+    } 
+     if(((x+y)%2==1) && (j<3)){
+      fill(255,0,0);
+      ellipse(x+size/2,y+size/2,size);
+    }
+    else if(((x+y)%2==1) && (j>4)){
+      fill(0);
+      ellipse(x+size/2,y+size/2,size);
+    }
     }
   }
-}
+  }
